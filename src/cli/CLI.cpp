386b55993cd1518c8493500f9b963b931cd6c348
anotherSimpleCoder <amsakanbavan@ymail.com>
2025-03-25 11:54:33 +0100

    Added error handling into CLI.

--- src/cli/CLI.cpp
+++ src/cli/CLI.cpp
@@ -1,13 +1,18 @@
 #include <string>
+#include <iostream>
 
 #include "CLI.h"
 #include "ASMPException.h"
 
 void CLI::run(int argc, char** argv) {
-    if (argc < 2) {
-        throw ASMPException("File is missing!");
-    }
+    try {
+        if (argc < 2) {
+            throw ASMPException("File is missing!");
+        }
 
-    std::string filename(argv[1]);
-    this->backend->play_file(filename);
+        std::string filename(argv[1]);
+        this->backend->play_file(filename);
+    } catch (ASMPException *exception) {
+        std::cerr << exception->what() << std::endl;
+    }
 }
