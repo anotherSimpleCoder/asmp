9892e900acac63617a8f7fa508b0f527d28d4a9d
anotherSimpleCoder <amsakanbavan@ymail.com>
2025-05-09 21:01:09 +0200

    Removed test for playing file as it includes io which doesn't make sense to be tested.

--- tests/audio_file_player/AudioFilePlayerTests.cpp
+++ tests/audio_file_player/AudioFilePlayerTests.cpp
@@ -3,13 +3,6 @@
 
 #include "InvalidAudioFileException.h"
 
-TEST(AudioFilePlayerTests, Runs) {
-    float data[10] = {0};
-    auto* testFile = new AudioFile("test.wav", data);
-    AudioFilePlayer testPlayer{};
-
-    EXPECT_NO_THROW(testPlayer.play(testFile));
-}
 
 TEST(AudioFilePlayerTests, PlayNullFile_ShouldThrowInvalidAudioFileException) {
     AudioFilePlayer testPlayer{};
